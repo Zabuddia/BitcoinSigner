8a2fde1f340340227fb6fa7090c57f54f58a9d83
Zabuddia <fife.alan@gmail.com>
2024-07-01 16:36:03 -0600

    Testing generating address from user input

--- menu.c
+++ menu.c
@@ -12,17 +12,17 @@ enum menu_state {
 static void draw_menu() {
     display_clear(BACKGROUND_COLOR);
     if (menu_state == STATE_GENERATE_ADDRESS) {
-        display_draw_string(STARTING_X, STARTING_Y, "Generate Address", DEFAULT_FONT, SELECTED_COLOR, FONT_COLOR);
+        display_draw_string(STARTING_X, STARTING_Y, "Generate Address", DEFAULT_FONT, SELECTED_BACKGROUND_COLOR, SELECTED_FONT_COLOR);
     } else {
         display_draw_string(STARTING_X, STARTING_Y, "Generate Address", DEFAULT_FONT, BACKGROUND_COLOR, FONT_COLOR);
     }
     if (menu_state == STATE_CHECK_BALANCE) {
-        display_draw_string(STARTING_X, STARTING_Y + 20, "Check Balance", DEFAULT_FONT, SELECTED_COLOR, FONT_COLOR);
+        display_draw_string(STARTING_X, STARTING_Y + 20, "Check Balance", DEFAULT_FONT, SELECTED_BACKGROUND_COLOR, SELECTED_FONT_COLOR);
     } else {
         display_draw_string(STARTING_X, STARTING_Y + 20, "Check Balance", DEFAULT_FONT, BACKGROUND_COLOR, FONT_COLOR);
     }
     if (menu_state == STATE_SEND_TRANSACTION) {
-        display_draw_string(STARTING_X, STARTING_Y + 40, "Send Transaction", DEFAULT_FONT, SELECTED_COLOR, FONT_COLOR);
+        display_draw_string(STARTING_X, STARTING_Y + 40, "Send Transaction", DEFAULT_FONT, SELECTED_BACKGROUND_COLOR, SELECTED_FONT_COLOR);
     } else {
         display_draw_string(STARTING_X, STARTING_Y + 40, "Send Transaction", DEFAULT_FONT, BACKGROUND_COLOR, FONT_COLOR);
     }
@@ -36,23 +36,23 @@ void menu_tick() {
     //Transitions
     switch (menu_state) {
         case STATE_GENERATE_ADDRESS:
-            if (button_up() != 0) {
+            if (button_up() == 0) {
                 menu_state = STATE_SEND_TRANSACTION;
-            } else if (button_down() != 0) {
+            } else if (button_down() == 0) {
                 menu_state = STATE_CHECK_BALANCE;
             }
             break;
         case STATE_CHECK_BALANCE:
-            if (button_up() != 0) {
+            if (button_up() == 0) {
                 menu_state = STATE_GENERATE_ADDRESS;
-            } else if (button_down() != 0) {
+            } else if (button_down() == 0) {
                 menu_state = STATE_SEND_TRANSACTION;
             }
             break;
         case STATE_SEND_TRANSACTION:
-            if (button_up() != 0) {
+            if (button_up() == 0) {
                 menu_state = STATE_CHECK_BALANCE;
-            } else if (button_down() != 0) {
+            } else if (button_down() == 0) {
                 menu_state = STATE_GENERATE_ADDRESS;
             }
             break;
